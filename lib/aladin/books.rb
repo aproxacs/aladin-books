9f100a413d51acf6042521435932391100b3b685
aproxacs <aproxacs.gmail.com>
2012-06-25 17:39:07 +0900

    Books class has been implemented

--- lib/aladin/books.rb
+++ lib/aladin/books.rb
@@ -1,4 +1,40 @@
 module Aladin
-  class Books
+  class Books < Array
+
+    class << self
+      attr_accessor :ttb_key, :per_page
+      def configure
+        yield self
+      end
+
+      def search(query)
+        uri = URI.parse('http://www.aladin.co.kr/ttb/api/ItemSearch.aspx')
+        params = {
+          :ttbkey => ttb_key,
+          :Query => query,
+          :QueryType => "Title",
+          :MaxResults => 10,
+          :start => 1,
+          :SearchTarget => "Book",
+          :output => "js"
+        }
+        uri.query = URI.encode_www_form(params)
+        res = Net::HTTP.get_response(uri)
+        if res.is_a?(Net::HTTPSuccess)
+          Books.new(JSON.parse(res.body.gsub(/;?/, "")))
+        end
+      end
+    end
+
+    attr_accessor :total, :page, :per_page
+
+    def initialize(hash)
+      @total = hash["totalResults"]
+      @page = 1 + (hash["startIndex"]/hash["itemsPerPage"])
+      @per_page = hash["itemsPerPage"]
+
+      super hash["item"].map {|i| Book.new(i)}
+    end
+
   end
 end
--- spec/aladin/books_spec.rb
+++ spec/aladin/books_spec.rb
@@ -2,8 +2,94 @@ require 'spec_helper'
 
 module Aladin
   describe Books do
-    it "does something" do
-      Book
+    let(:ttb_key) { "TTB_Key" }
+
+    describe ".configure" do
+      it "sets ttb_key" do
+        Books.configure do |config|
+          config.ttb_key = ttb_key
+        end
+
+        Books.ttb_key.should == ttb_key
+      end
+
+      it "sets per_page" do
+        Books.configure do |config|
+          config.per_page = 20
+        end
+
+        Books.per_page.should == 20
+      end
     end
+
+    describe ".search" do
+      let(:query) { "hello" }
+      let(:body) { File.read(File.join(File.dirname(__FILE__), "../data/hello_result.txt")) }
+      before(:each) do
+        Books.configure {|c| c.ttb_key = ttb_key}
+
+        stub_request(:get, "http://www.aladin.co.kr/ttb/api/ItemSearch.aspx")
+            .with(:query => hash_including(:Query => query))
+            .to_return(:body => body)
+      end
+
+      it "returns books" do
+        books = Books.search query
+
+        books.should be_a(Books)
+      end
+
+      it "sends request to aladin" do
+        Books.search query
+        a_request(:get, "http://www.aladin.co.kr/ttb/api/ItemSearch.aspx")
+            .with(:query => hash_including(:Query => query)).should have_been_made
+      end
+
+      it "creates books object with respond body" do
+        # Response from aladin ends with ';'
+        # so we need to remove this last characator in order to parse json string smoothly
+        hash = JSON.parse(body.gsub(/;?/, ""))
+
+        Books.should_receive(:new).with(hash)
+        Books.search query
+      end
+
+      # it "real connection test" do
+      #   WebMock.allow_net_connect!
+      #   Books.ttb_key = ""
+      #   Books.search "ruby"
+      # end
+    end
+
+
+    describe "#initialize", focus: true do
+      let(:body) { File.read(File.join(File.dirname(__FILE__), "../data/ruby_result.txt")) }
+      let(:hash) { JSON.parse(body.gsub(/;?/, "")) }
+      let(:books) { Books.new(hash) }
+
+      it "assigns a total attribute" do
+        books.total.should == 1753
+      end
+
+      it "assigns a page attribute" do
+        books.page.should == 1
+      end
+
+      it "assigns a per_page attribute" do
+        books.per_page.should == 10
+      end
+
+      it "creates 10 books" do
+        Book.should_receive(:new).exactly(10)
+        books
+      end
+
+      it "assigns books array" do
+        books.length.should == 10
+        books.should be_a(Array)
+        books.first.should be_a(Book)
+      end
+    end
+
   end
 end
