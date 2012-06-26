cf6e71b73c37e464f11488b7f9fb62880349c5bb
aproxacs <aproxacs.gmail.com>
2012-06-26 12:13:18 +0900

    refactoring Books class

--- lib/aladin/books.rb
+++ lib/aladin/books.rb
@@ -8,38 +8,40 @@ module Aladin
       end
 
       def search(query)
-        uri = URI.parse('http://www.aladin.co.kr/ttb/api/ItemSearch.aspx')
-        params = {
-          :ttbkey => ttb_key,
-          :Query => query,
-          :QueryType => "Title",
-          :MaxResults => 10,
-          :start => 1,
-          :SearchTarget => "Book",
-          :output => "js"
-        }
-        uri.query = URI.encode_www_form(params)
+        uri.query = URI.encode_www_form(default_params.update(:Query => query))
         res = Net::HTTP.get_response(uri)
         if res.is_a?(Net::HTTPSuccess)
-
           body = res.body.gsub(/;?/, "")
           hash = JSON.parse(body) rescue JSON.parse(body.gsub("'", '"'))
 
           Books.new(hash)
         end
       end
+
+      private
+        def uri
+          @uri ||= URI.parse('http://www.aladin.co.kr/ttb/api/ItemSearch.aspx')
+        end
+
+        def default_params
+          @default_params ||= {
+            :ttbkey => ttb_key,
+            :QueryType => "Title",
+            :MaxResults => per_page,
+            :start => 1,
+            :SearchTarget => "Book",
+            :output => "js"
+          }
+        end
     end
 
     attr_accessor :total, :page, :per_page, :error_code, :error_msg
 
     def initialize(hash)
       if hash["errorCode"]
-        @error_code = hash["errorCode"]
-        @error_msg = hash["errorMessage"]
+        set_error(hash)
       else
-        @total = hash["totalResults"]
-        @page = 1 + (hash["startIndex"]/hash["itemsPerPage"])
-        @per_page = hash["itemsPerPage"]
+        set_page_info(hash)
 
         super hash["item"].map {|i| Book.new(i)}
       end
@@ -48,6 +50,18 @@ module Aladin
     def error?
       @error_code.present?
     end
+
+    private
+      def set_error(hash)
+        @error_code = hash["errorCode"]
+        @error_msg = hash["errorMessage"]
+      end
+
+      def set_page_info(hash)
+        @total = hash["totalResults"]
+        @page = 1 + (hash["startIndex"]/hash["itemsPerPage"])
+        @per_page = hash["itemsPerPage"]
+      end
 
   end
 end
